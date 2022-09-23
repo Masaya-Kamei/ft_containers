7823a484a1f22a26c56d3efec05199378f62e087
mkamei <mkamei@student.42tokyo.jp>
2022-09-23 12:52:12 +0900

    Add stack

---
+++ srcs/stack.hpp
@@ -0,0 +1,77 @@
+#ifndef STACK_HPP
+# define STACK_HPP
+
+# include "vector.hpp"
+
+namespace ft
+{
+
+template<class T, class Container = ft::vector<T> >
+class stack
+{
+	public:
+		typedef T								value_type;
+		typedef Container						container_type;
+		typedef typename Container::size_type	size_type;
+
+	protected:
+		container_type	c_;
+
+	public:
+		explicit stack(const container_type& ctnr = container_type()) : c_(ctnr) {}
+		~stack() {}
+		stack& operator=(const stack& rhs) { c_ = rhs.c_; return (*this); }
+
+		bool 				empty() const				{ return (c_.empty()); }
+		size_type 			size()	const				{ return (c_.size()); }
+		value_type& 		top()			  			{ return (c_.back()); }
+		const value_type&	top() 	const				{ return (c_.back()); }
+		void				push(const value_type& val)	{ c_.push_back(val); }
+		void 				pop()						{ c_.pop_back(); }
+
+		template<class T1, class Container1>
+		friend bool	operator==(const stack<T1, Container1>& lhs, const stack<T1, Container1>& rhs);
+
+		template<class T1, class Container1>
+		friend bool	operator<(const stack<T1, Container1>& lhs, const stack<T1, Container1>& rhs);
+};
+
+template<class T, class Container>
+bool	operator==(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (lhs.c_ == rhs.c_);
+}
+
+template<class T, class Container>
+bool	operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (lhs.c_ < rhs.c_);
+}
+
+template<class T, class Container>
+bool	operator!=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (!(lhs.c_ == rhs.c_));
+}
+
+template<class T, class Container>
+bool	operator<=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (!(rhs.c_ < lhs.c_));
+}
+
+template<class T, class Container>
+bool	operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (rhs.c_ < lhs.c_);
+}
+
+template<class T, class Container>
+bool	operator>=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+{
+	return (!(lhs.c_ < rhs.c_));
+}
+
+}  // namespace ft
+
+#endif  // STACK_HPP
---
+++ tests/stack_test.cpp
@@ -0,0 +1,157 @@
+#ifndef TESTLIB
+# include <gtest/gtest.h>
+#else
+# include "testlib.hpp"
+#endif
+#include <stack>
+#include <vector>
+#include "stack.hpp"
+#include "vector.hpp"
+
+class StackTest : public ::testing::Test
+{
+	protected:
+		static void SetUpTestCase()
+		{
+		}
+		static void TearDownTestCase()
+		{
+		}
+		virtual void SetUp()
+		{
+			st_stack_.push(10); st_stack_.push(20); st_stack_.push(30);
+			ft_stack_.push(10); ft_stack_.push(20); ft_stack_.push(30);
+		}
+		virtual void TearDown()
+		{
+		}
+
+		template <class T, class Container1, class Container2>
+		void CompareSizeTop(
+			const std::stack<T, Container1>& st_stack
+			, const ft::stack<T, Container2>& ft_stack)
+		{
+			EXPECT_EQ(st_stack.size(),	ft_stack.size());
+			if (st_stack.size() > 0)
+				EXPECT_EQ(st_stack.top(), 	ft_stack.top());
+		}
+
+		template <class Stack>
+		void PrintStack(const Stack& stack)
+		{
+			std::cout << "size:  "	<< stack.size() << std::endl;
+			std::cout << "top:   "	<< stack.top() << std::endl;
+			std::cout << "empty: "	<< stack.empty() << std::endl;
+		}
+
+		static const int								nums[3];
+		static const std::vector<int>					st_const_vec_;
+		static const ft::vector<int>					ft_const_vec_;
+		static const std::stack<int, std::vector<int> >	st_const_stack_;
+		static const ft::stack<int, ft::vector<int> >	ft_const_stack_;
+		std::stack<int, std::vector<int> >				st_stack_;
+		ft::stack<int, ft::vector<int> >				ft_stack_;
+		std::stack<int, std::vector<int> >				st_zero_stack_;
+		ft::stack<int, ft::vector<int> >				ft_zero_stack_;
+};
+
+const int				StackTest::nums[3] = {10, 20, 30};
+const std::vector<int>	StackTest::st_const_vec_(nums, nums + 3);
+const ft::vector<int>	StackTest::ft_const_vec_(nums, nums + 3);
+const std::stack<int, std::vector<int> >	StackTest::st_const_stack_(st_const_vec_);
+const ft::stack<int, ft::vector<int> >		StackTest::ft_const_stack_(ft_const_vec_);
+
+TEST_F(StackTest, DefaultConstructorTest)
+{
+	std::stack<int, std::vector<int> >	st_stack;
+	ft::stack<int, ft::vector<int> >	ft_stack;
+	CompareSizeTop(st_stack, ft_stack);
+}
+
+TEST_F(StackTest, CopyConstructorTest)
+{
+	std::stack<int, std::vector<int> >	st_stack(st_stack_);
+	ft::stack<int, ft::vector<int> >	ft_stack(ft_stack_);
+	CompareSizeTop(st_stack, ft_stack);
+
+	std::stack<int, std::vector<int> >	st_zero_stack(st_zero_stack_);
+	ft::stack<int, ft::vector<int> >	ft_zero_stack(ft_zero_stack_);
+	CompareSizeTop(st_zero_stack, ft_zero_stack);
+
+	std::stack<int, std::vector<int> >	st_vec_stack(st_const_vec_);
+	ft::stack<int, ft::vector<int> >	ft_vec_stack(ft_const_vec_);
+	CompareSizeTop(st_vec_stack, ft_vec_stack);
+}
+
+TEST_F(StackTest, OpeAssignTest)
+{
+	std::stack<int, std::vector<int> >	st_stack;	st_stack = st_stack_;
+	ft::stack<int, ft::vector<int> >	ft_stack;	ft_stack = ft_stack_;
+	CompareSizeTop(st_stack, ft_stack);
+
+	std::stack<int, std::vector<int> >	st_zero_stack;	st_zero_stack = st_zero_stack_;
+	ft::stack<int, ft::vector<int> >	ft_zero_stack;	ft_zero_stack = ft_zero_stack_;
+	CompareSizeTop(st_zero_stack, ft_zero_stack);
+
+	// std::stack<int, std::vector<int> >	st_vec_stack;	st_vec_stack = st_const_vec_;
+	// ft::stack<int, ft::vector<int> >	ft_vec_stack;	ft_vec_stack = ft_const_vec_;
+	// CompareSizeTop(st_vec_stack, ft_vec_stack);
+}
+
+TEST_F(StackTest, EmptyTest)
+{
+	EXPECT_EQ(st_stack_.empty(), ft_stack_.empty());
+	EXPECT_EQ(st_zero_stack_.empty(), ft_zero_stack_.empty());
+	EXPECT_EQ(st_const_stack_.empty(), ft_const_stack_.empty());
+
+	st_stack_.pop(); st_stack_.pop(); st_stack_.pop();
+	ft_stack_.pop(); ft_stack_.pop(); ft_stack_.pop();
+	EXPECT_EQ(st_stack_.empty(), ft_stack_.empty());
+}
+
+TEST_F(StackTest, SizeTest)
+{
+	EXPECT_EQ(st_stack_.size(), ft_stack_.size());
+	EXPECT_EQ(st_zero_stack_.size(), ft_zero_stack_.size());
+	EXPECT_EQ(st_const_stack_.size(), ft_const_stack_.size());
+}
+
+TEST_F(StackTest, TopTest)
+{
+	EXPECT_EQ(st_stack_.top(), ft_stack_.top());
+	EXPECT_EQ(st_const_stack_.top(), ft_const_stack_.top());
+	// EXPECT_EQ(st_zero_stack_.top(), ft_zero_stack_.top());
+
+	st_stack_.push(42);
+	ft_stack_.push(42);
+	EXPECT_EQ(st_stack_.top(), ft_stack_.top());
+	st_stack_.pop();
+	ft_stack_.pop();
+	EXPECT_EQ(st_stack_.top(), ft_stack_.top());
+}
+
+TEST_F(StackTest, PushTest)
+{
+	st_stack_.push(42);
+	ft_stack_.push(42);
+	CompareSizeTop(st_stack_, ft_stack_);
+	st_zero_stack_.push(42);
+	ft_zero_stack_.push(42);
+	CompareSizeTop(st_zero_stack_, ft_zero_stack_);
+}
+
+TEST_F(StackTest, PopTest)
+{
+	st_stack_.pop();
+	ft_stack_.pop();
+	CompareSizeTop(st_stack_, ft_stack_);
+	st_stack_.pop();
+	ft_stack_.pop();
+	CompareSizeTop(st_stack_, ft_stack_);
+	st_stack_.pop();
+	ft_stack_.pop();
+	CompareSizeTop(st_stack_, ft_stack_);
+	// st_stack_.pop();
+	// ft_stack_.pop();
+	// CompareSizeTop(st_stack_, ft_stack_);
+}
